2aebc003607c9226e4d69eb4f5857c220bbd2604
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-06-17 16:29:43 +0530

    home page category added below swiper

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import HomeSlider from '../components/HomeSlider/page'
+import HomeCategory from '../components/HomeCategory/page'
 export default function Home() {
   return <>
     <HomeSlider/>
+    <HomeCategory />
   </>;
 }
---
+++ components/HomeCategory/page.js
@@ -0,0 +1,38 @@
+import Image from "next/image";
+import style from "./style.module.css";
+
+export default function page() {
+  return (
+    <div>
+      <div className={style.category_cards}>
+        <div className={style.card}>
+          <div className={style.imageSection}>
+            <Image src="/homecard1.webp" alt="home card 1" layout="fill" />
+          </div>
+          <h2>Headphone</h2>
+        </div>
+
+        <div className={style.card}>
+          <div className={style.imageSection}>
+            <Image src="/homecard2.webp" alt="home card 1" layout="fill" />
+          </div>
+          <h2>Earphone</h2>
+        </div>
+
+        <div className={style.card}>
+          <div className={style.imageSection}>
+            <Image src="/homecard3.webp" alt="home card 1" layout="fill" />
+          </div>
+          <h2>Smartphone</h2>
+        </div>
+
+        <div className={style.card}>
+          <div className={style.imageSection}>
+            <Image src="/homecard4.webp" alt="home card 1" layout="fill" />
+          </div>
+          <h2>Smartwatch</h2>
+        </div>
+      </div>
+    </div>
+  );
+}
