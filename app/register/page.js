bdd148e1476896c272914af964c76c43b3fd2280
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-06-17 11:33:43 +0530

    login and regsiter page created

---
+++ app/register/page.js
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import style from '../login/style.module.css'
+export default function page() {
+  return (
+    <div className={style.login}>
+     <div className={style.form_container}>
+<h1>Create Account</h1>
+<p>Please Register using account detail bellow.</p>
+
+<form>
+
+    <input type="text" placeholder='First Name' required/>
+    <input type="text" placeholder='Last Name' required/>
+    <input type="email" placeholder='Email' required/>
+    <input type="password" placeholder='Password' required/>
+    <div className={style.btn_section}>
+<button>Create</button>
+
+    </div>
+</form>
+
+
+{/* create account */}
+<div className={style.createAccount}>
+<Link href="/login">
+Login account
+</Link>
+</div>
+
+     </div>
+    </div>
+  )
+}
--- layout/Header/page.js
+++ layout/Header/page.js
@@ -1,5 +1,8 @@
 import Image from "next/image";
-
+import { IoSearch } from "react-icons/io5";
+import { FaRegUser } from "react-icons/fa";
+import { FiShoppingCart } from "react-icons/fi";
+import Link from "next/link";
 export default function page() {
   return (
     <header>
@@ -13,10 +16,17 @@ export default function page() {
 
       {/* links */}
       <div className="links_container">
-<li>Home</li>
-<li>Shop</li>
-<li>Category</li>
-<li>Product</li>
+<li><Link href={""}>Home</Link></li>
+<li><Link href={""}>Shop</Link></li>
+<li><Link href={""}>Category</Link></li>
+<li><Link href={""}>Product</Link></li>
+      </div>
+
+      {/* right section */}
+      <div className="header_right">
+        <li><i><IoSearch/></i></li>
+        <li><i><Link href="/register"><FaRegUser/></Link></i></li>
+        <li><i><Link href="/"><span>0</span><FiShoppingCart/></Link></i></li>
       </div>
     </header>
   );
