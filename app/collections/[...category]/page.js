5f7d2896fca943f44eab414290588c5082a344c5
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-06-18 14:05:12 +0530

    card image created

--- app/collections/[...category]/page.js
+++ app/collections/[...category]/page.js
@@ -2,8 +2,10 @@
 import { FaAngleDown } from "react-icons/fa";
 import { FaChevronRight } from "react-icons/fa";
 import style from "./style.module.css";
-import React, { useEffect, useState } from "react";
+import { FaRegEye } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
 import { useParams } from "next/navigation";
+import { FaShoppingCart } from "react-icons/fa";
 import Image from "next/legacy/image";
 import Link from "next/link";
 export default function Page() {
@@ -125,7 +127,85 @@ export default function Page() {
           </div>
         </div>
 
-        <div className={style.itemCategory}></div>
+        <div className={style.itemCategory}>
+          {/* top bar */}
+          <div className={style.topBar}>
+            <div className={style.search}>
+              <select>
+                <option value="manual">Featured</option>
+                <option value="best-selling">Best Selling</option>
+                <option value="title-ascending">Alphabetically, A-Z</option>
+                <option value="title-descending">Alphabetically, Z-A</option>
+                <option value="price-ascending">Price, low to high</option>
+                <option value="price-descending">Price, high to low</option>
+                <option value="created-descending">Date, new to old</option>
+                <option value="created-ascending">Date, old to new</option>
+              </select>
+            </div>
+
+            <div className={style.showingResult}>Showing 1 - 9 of 9 result</div>
+          </div>
+
+          {/* items container */}
+          <div className={style.itemContainerBody}>
+            <div className={style.itemCard}>
+              <div className={style.itemCardImageContainer}>
+                <Image src="/dummy/img1.webp" alt="img1" layout="fill" />
+                {/* menu on image */}
+                <div className={style.menuOnImage}>New</div>
+
+                {/* image drop down */}
+                <div className={style.imageDropDown}>
+                  <li title="Quick View">
+                    <Link href="">
+                      <FaRegEye />
+                    </Link>
+                  </li>
+                  <li title="Add to cart">
+                    <Link href="">
+                      <FaShoppingCart />
+                    </Link>
+                  </li>
+                  <li title="Add to wishlist">
+                    <Link href="">
+                      <FaHeart />
+                    </Link>
+                  </li>
+                </div>
+              </div>
+
+              <div className={style.productDesc}>
+                <h2>Zk. Women Short T Shirt</h2>
+                <p>Rs. 6,800.00</p>
+
+                <div className={style.colors}>
+                  <div
+                    className={style.color + " " + style.red_color}
+                    title="Red"
+                  ></div>
+                  <div
+                    className={style.blue_color + " " + style.color}
+                    title="Blue"
+                  ></div>
+                  <div
+                    className={style.green_color + " " + style.color}
+                    title="Green"
+                  ></div>
+                  <div
+                    className={style.pink_color + " " + style.color}
+                    title="Pink"
+                  ></div>
+                  <div
+                    className={style.no_color + " " + style.color}
+                    title="More"
+                  >
+                    +1
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
