41440280f9d48ac9b01c4d207c80090e16a5d9ae
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-06-18 17:52:55 +0530

    cart pop page created

--- components/CartPage/page.js
+++ components/CartPage/page.js
@@ -1,6 +1,8 @@
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 import { IoMdClose } from "react-icons/io";
+import { MdClose } from "react-icons/md";
 export default function page() {
   return (
     <div className='cart_container'>
@@ -12,6 +14,20 @@ export default function page() {
       <h2>Cart <span className='close' title="close cart"><IoMdClose /></span></h2>
 
 <div className="item_container">
+
+<li>
+<div className="item_image">
+<div className="deleteItems" title='delete'>
+    <MdClose/>
+</div>
+    <Image src="/dummy/img1.webp" alt="img 1" layout='fill'/>
+</div>
+<div className="item_details">
+    <h1>W. Men Formal T-shirt - white</h1>
+    <p>1 x Rs. 1,700.00</p>
+</div>
+</li>
+
 
 </div>
 
