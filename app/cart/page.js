335e897272b48128add949ffaf941d4cb5307e0c
suraj singh rajput <67959669+surajsinghgore@users.noreply.github.com>
2024-06-19 11:10:22 +0530

    cart page almost done

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,9 +1,39 @@
-import style from './style.module.css'
+import style from "./style.module.css";
 
 export default function page() {
   return (
-    <div>
-      dewrewrw
+    <div className={style.cartModelContainer}>
+      <h1>Your Shopping Cart</h1>
+
+      <div className={style.cartItemParent}></div>
+
+      <div className={style.bottomBtn}>
+        <button>Continue Shopping</button>
+        <button>Clean cart</button>
+      </div>
+
+      {/* bottom section */}
+
+      <div className={style.bottomSection}>
+        <div className={style.left}>
+          <h4>Special instructions for seller</h4>
+          <textarea></textarea>
+        </div>
+        {/* right */}
+        <div className={style.right}>
+          <h4>Cart Totals</h4>
+          <div className={style.tableSection}>
+            <li className={style.heading}>Subtotal</li>
+            <li className={style.data}>Rs. 32,000.00</li>
+
+            <li className={style.bottomHeading}>Total</li>
+            <li className={style.bottomData}>Rs. 32,000.00</li>
+
+          </div>
+
+          <button>Proceed to checkout</button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
