185e13ae6cfbc3514431b13333b58e389e0089a8
suraj singh rajput <surajthakurrs45@gmail.com>
2024-06-24 16:16:14 +0530

    checkout page created

--- app/checkout/page.js
+++ app/checkout/page.js
@@ -63,21 +63,84 @@ export default function page() {
 
           <div className={style.payment}>
             <li>
-              <input type="radio" id="online" />
+              <input type="radio" name="paymentmode" />
               <span>Credit card</span>
             </li>
             <li className={style.cardDetails}>
               <div className={style.formInput}>
-                <label htmlFor="cardNumber">Country/Region</label>
                 <input type="text" placeholder="Card number" id="cardNumber" />
+                <input type="text" placeholder="Expiration date (MM/YY)" className={style.half}/>
+                <input type="text" placeholder="Security Code" className={style.half}/>
+                <input type="text" placeholder="Name on Card" />
               </div>
             </li>
+
+
+
+            <li>
+              <input type="radio" name="paymentmode" id="" />
+              <span>Cash on Delivery (COD)</span>
+            </li>
+          </div>
+
+
+          <div className={style.payNow}>
+<button type="submit">Pay Now</button>
+
           </div>
         </form>
       </div>
 
       {/* item container */}
-      <div className={style.itemContainer}></div>
+      <div className={style.itemContainer}>
+      <div className={style.itemMainContainer}>
+
+
+        <div className={style.items}>
+        <div className={style.imageContainer}>
+<div className={style.qty}>1</div>
+          <img src="cart1.webp" alt="cart dummy image" />
+        </div>
+        <div className={style.titleOfItem}>
+        Aa. Men Coat
+        </div>
+        <div className={style.rs}>
+        $117.00
+        </div>
+        </div>
+
+        <div className={style.items}>
+        <div className={style.imageContainer}>
+<div className={style.qty}>1</div>
+          <img src="cart1.webp" alt="cart dummy image" />
+        </div>
+        <div className={style.titleOfItem}>
+        Aa. Men Coat
+        </div>
+        <div className={style.rs}>
+        $117.00
+        </div>
+        </div>
+
+        </div>
+
+        {/* bottom total */}
+        <div className={style.bottomTotal}>
+<div className={style.discount}>
+<input type="text" name="" placeholder="Discount code or gift card" />
+<button>Apply</button>
+</div>
+<div className={style.price}>
+<li><span >Subtotal</span> <span >$627.00</span></li>
+
+<li><span >Shipping</span> <span >$0.15</span></li>
+
+<li><span className={style.heading}>Total</span> <span className={style.priceTotal}>
+$627.15</span></li>
+</div>
+
+        </div>
+      </div>
     </div>
   );
 }
