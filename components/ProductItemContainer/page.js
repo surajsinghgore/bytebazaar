687e1b76595ae628327c16fc432171f11bd4a97f
suraj singh rajput <surajthakurrs45@gmail.com>
2024-06-26 18:01:56 +0530

    wishlist icon added in cart

--- components/ProductItemContainer/page.js
+++ components/ProductItemContainer/page.js
@@ -6,74 +6,70 @@ import { FaShoppingCart } from "react-icons/fa";
 import Image from "next/legacy/image";
 import { useCart } from "react-use-cart";
 import { useEffect, useState } from "react";
-import { ToastContainer, toast } from "react-toastify";
+import { FaHeart } from "react-icons/fa";
+import { toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 export default function Page({ item }) {
   const [inCartState, setInCartState] = useState(true);
   const { items, removeItem, addItem } = useCart();
 
-
-  const checkInCart=()=>{
+  const checkInCart = () => {
     let dataCount = items.filter((cartItem) => {
-        return cartItem.id.toLowerCase()==item._id;
-      });
-      if(dataCount.length===1){
-        setInCartState(true)
-      }else{
-        setInCartState(false)
-      }
-
-  }
+      return cartItem.id.toLowerCase() == item._id;
+    });
+    if (dataCount.length === 1) {
+      setInCartState(true);
+    } else {
+      setInCartState(false);
+    }
+  };
   const addProductToCart = (product) => {
-
-      let productName = product.name;
-      let productImage = product.image;
-      let productPrice = product.price;
-      let id = product._id;
-      let QtyBook = 1;
-      addItem({
-          id,
-          productName,
-          productImage,
-          price: productPrice,
-          qtyBook: QtyBook,
-        });
-        checkInCart()
-        toast.success(`${productName} successfully added in cart`, {
-            position: "bottom-right",
-            autoClose: 2000,
-            hideProgressBar: false,
-            closeOnClick: true,
-            pauseOnHover: true,
-            draggable: true,
-            progress: undefined,
-          });
+    let productName = product.name;
+    let productImage = product.image;
+    let productPrice = product.price;
+    let id = product._id;
+    let QtyBook = 1;
+    addItem({
+      id,
+      productName,
+      productImage,
+      price: productPrice,
+      qtyBook: QtyBook,
+    });
+    checkInCart();
+    toast.success(`${productName} successfully added in cart`, {
+      position: "bottom-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
   };
 
-  const removeFromCart = (id,name) => {
-      removeItem(id);
-      checkInCart()
-      toast.error(`${name} successfully removed from cart`, {
-        position: "bottom-right",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-      });
+  const removeFromCart = (id, name) => {
+    removeItem(id);
+    checkInCart();
+    toast.error(`${name} successfully removed from cart`, {
+      position: "bottom-right",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
   };
 
-
-useEffect(()=>{
+  useEffect(() => {
     let dataCount = items.filter((cartItem) => {
-        return cartItem.id.toLowerCase()==item._id;
-      });
-      if(dataCount.length===1){
-          setInCartState(false)
-      }
-
-},[])
+      return cartItem.id.toLowerCase() == item._id;
+    });
+    if (dataCount.length === 1) {
+      setInCartState(false);
+    }
+  }, []);
 
   return (
     <div className={style.itemCard} key={item._id}>
@@ -97,12 +93,16 @@ useEffect(()=>{
             <>
               <li
                 title="Remove From Cart"
-                onClick={() => removeFromCart(item._id,item.name)}
+                onClick={() => removeFromCart(item._id, item.name)}
               >
                 <MdRemoveShoppingCart />
               </li>
             </>
           )}
+
+          <li title="Add to wishlist">
+            <FaHeart />
+          </li>
         </div>
       </div>
 
@@ -110,7 +110,6 @@ useEffect(()=>{
         <h2>{item.name}</h2>
         <p>Rs. {item.price}</p>
       </div>
-      
     </div>
   );
 }
