78cfb2188246118b8773488bd49ebba815f7c178
suraj singh rajput <surajthakurrs45@gmail.com>
2024-07-05 12:04:54 +0530

    checkout page dynamic

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,44 +1,22 @@
-"use client"
+"use client";
 import Image from "next/legacy/image";
 import style from "./style.module.css";
 import { useCart } from "react-use-cart";
 import Link from "next/link";
-import { ToastContainer, toast } from "react-toastify";
 import { IoMdClose } from "react-icons/io";
+import {  toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { useEffect, useState } from "react";
 export default function page() {
-  const { emptyCart, removeItem, items,updateItem } = useCart();
+  const { emptyCart, removeItem, items, updateItem } = useCart();
+  const [specialInstruction, SetSpecialInstruction] = useState("");
+  const [payableAmount, setPayableAmount] = useState(0);
 
-  const [payableAmount,setPayableAmount]=useState(0);
-
-
-
-// clean cart
-  const cleanAllCartItem=()=>{
+  // clean cart
+  const cleanAllCartItem = () => {
     emptyCart();
-    setPayableAmount(0)
-     toast.error('Cart Clean Successfully', {
-          position: "bottom-right",
-          autoClose: 1200,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-        });
-   
-    }
-
-// delete item
-const deleteItem=(item)=>{
-  removeItem(item.id);
-if(items.length==1){
-setPayableAmount(0)
-
-}
-
-toast.error(`Item successfully removed from cart`, {
+    setPayableAmount(0);
+    toast.error("Cart Clean Successfully", {
       position: "bottom-right",
       autoClose: 1200,
       hideProgressBar: false,
@@ -47,61 +25,74 @@ toast.error(`Item successfully removed from cart`, {
       draggable: true,
       progress: undefined,
     });
-    
-}
+  };
+
+  // delete item
+  const deleteItem = (item) => {
+    removeItem(item.id);
+    if (items.length == 1) {
+      setPayableAmount(0);
+    }
+
+    toast.error(`Item successfully removed from cart`, {
+      position: "bottom-right",
+      autoClose: 1200,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+    });
+  };
 
   // decrement
-const decrement=(item)=>{
-  let book=parseInt(item.qtyBook);
-  let book1=--book;
-  if(book1!==0){
-  let qb=parseInt(--item.qtyBook)
-  let tm=parseInt(item.price*item.qtyBook)
-  updateItem(item.id,{
-  qtyBook:qb,
-  itemTotal:tm
-  })
-  
-  }
-  else{
-   deleteItem(item)
-  
-  }
-  }
-  
-  
+  const decrement = (item) => {
+    let book = parseInt(item.qtyBook);
+    let book1 = --book;
+    if (book1 !== 0) {
+      let qb = parseInt(--item.qtyBook);
+      let tm = parseInt(item.price * item.qtyBook);
+      updateItem(item.id, {
+        qtyBook: qb,
+        itemTotal: tm,
+      });
+    } else {
+      deleteItem(item);
+    }
+  };
 
   // increment
-const increment=(item)=>{
+  const increment = (item) => {
+    let qb = parseInt(++item.qtyBook);
+    let tAmt = parseInt(item.price * item.qtyBook);
+    updateItem(item.id, {
+      qtyBook: qb,
+      totalAmount: tAmt,
+    });
+  };
 
-let qb=parseInt(++item.qtyBook)
-let tAmt=parseInt(item.price*item.qtyBook)
-updateItem(item.id,{
-qtyBook:qb,
-totalAmount:tAmt
-})
+  // total amount calculate
+  useEffect(() => {
+    setPayableAmount(0);
+    let sum = 0;
+    for (let i = 0; i < items.length; i++) {
+      let number = parseInt(items[i].qtyBook * items[i].price);
+      sum += number;
+    }
+    setPayableAmount(sum);
+  }, [decrement, increment, cleanAllCartItem, deleteItem]);
 
-
-
-}
-
-useEffect(()=>{
-  setPayableAmount(0)
-  let sum=0;
-  for (let i = 0; i < items.length; i++) {
-    let number=parseInt(items[i].qtyBook*items[i].price)
-    sum+=number;
-}
-    setPayableAmount(sum)
-},[decrement,increment,cleanAllCartItem,deleteItem])
-
+  // handle instruction
+  const handleInstruction = (e) => {
+    SetSpecialInstruction(e.target.value);
+  };
 
   return (
     <div className={style.cartModelContainer}>
       <h1>Your Shopping Cart</h1>
 
-{/* if not item in cart then show this */}
-{/* <div className={style.NoItemInCart}>
+      {/* if not item in cart then show this */}
+      {/* <div className={style.NoItemInCart}>
 
 <h2>Shopping Cart</h2>
 <h3>Your cart is currently empty.</h3>
@@ -111,85 +102,119 @@ Continue browsing <Link href="/">here</Link>
 </p>
 </div> */}
 
-{/* if item in cart */}
-<div className={style.ItemInShopCart}>
+      {/* if item in cart */}
+      <div className={style.ItemInShopCart}>
+        {/* cart items section */}
+        <div className={style.cartItemParent}>
+          {items.length != 0 ? (
+            <>
+              {items.map((item) => {
+                return (
+                  <div className={style.itemSection} key={item.id}>
+                    {/* img */}
+                    <div className={style.itemImg}>
+                      <Image
+                        src={item.productImage}
+                        alt={item.productImage}
+                        layout="fill"
+                      />
+                    </div>
+                    {/* productName */}
+                    <div className={style.productName}>
+                      <Link href="">{item.productName}</Link>
+                    </div>
 
+                    {/* price */}
 
-      {/* cart items section */}
-      <div className={style.cartItemParent}>
+                    <div className={style.price}>{item.price}</div>
+                    {/* qty control */}
+                    <div className={style.qtyControl}>
+                      <div
+                        className={style.min}
+                        onClick={() => decrement(item)}
+                      >
+                        -
+                      </div>
+                      <div className={style.number}>{item.qtyBook}</div>
+                      <div
+                        className={style.max}
+                        onClick={() => increment(item)}
+                      >
+                        +
+                      </div>
+                    </div>
 
+                    {/* total */}
 
-      {items.length != 0 ?
-      <>
-        {items.map((item)=>{
-          return   <div className={style.itemSection} key={item.id}>
-          {/* img */}
-          <div className={style.itemImg}>
-            <Image src={item.productImage} alt={item.productImage} layout="fill" />
+                    <div className={style.price}>
+                      {item.price * item.qtyBook}
+                    </div>
+
+                    {/* remove  */}
+                    <div className={style.remove}>
+                      <i>
+                        <IoMdClose
+                          onClick={() => deleteItem(item)}
+                          title="remove"
+                        />
+                      </i>
+                    </div>
+                  </div>
+                );
+              })}
+            </>
+          ) : (
+            <h1>Your cart is currently empty.</h1>
+          )}
+          {/* item1 */}
+        </div>
+
+        <div className={style.bottomBtn}>
+          <Link href={"/products"}>
+            {" "}
+            <button>Continue Shopping</button>
+          </Link>
+          <button onClick={() => cleanAllCartItem()}>Clean cart</button>
+        </div>
+
+        {/* bottom section */}
+
+        <div className={style.bottomSection}>
+          <div className={style.left}>
+            <h4>Special instructions for seller</h4>
+            <textarea
+              name="instruction"
+              value={specialInstruction}
+              onChange={(e) => handleInstruction(e)}
+            ></textarea>
           </div>
-          {/* productName */}
-          <div className={style.productName}>
-            <Link href="">{item.productName}</Link>
-          </div>
+          {/* right */}
+          <div className={style.right}>
+            <h4>Cart Totals</h4>
+            <div className={style.tableSection}>
+              <li className={style.heading}>Subtotal</li>
+              <li className={style.data}>Rs. {payableAmount}</li>
 
-          {/* price */}
-
-          <div className={style.price}>{item.price}</div>
-          {/* qty control */}
-          <div className={style.qtyControl}>
-            <div className={style.min} onClick={()=>decrement(item)}>-</div>
-            <div className={style.number}>{item.qtyBook}</div>
-            <div className={style.max}  onClick={()=>increment(item)}>+</div>
-          </div>
-
-          {/* total */}
-
-          <div className={style.price}>{item.price*item.qtyBook}</div>
-
-          {/* remove  */}
-          <div className={style.remove}  >
-            <i>
-              <IoMdClose onClick={()=>deleteItem(item)} title="remove" />
-            </i>
+              <li className={style.bottomHeading}>Total</li>
+              <li className={style.bottomData}>Rs. {payableAmount}</li>
+            </div>
+            {console.log(items.length)}
+            {items.length == 0 ? (
+              <>
+                <button disabled style={{ cursor: "text" }}>
+                  Proceed to checkout
+                </button>
+              </>
+            ) : (
+              <>
+                <Link href="/checkout">
+                  <button>Proceed to checkout</button>
+                </Link>
+              </>
+            )}
           </div>
         </div>
-        })}
-      </>:<h1>Your cart is currently empty.</h1>}
-        {/* item1 */}
-      
-
-    
       </div>
-
-      <div className={style.bottomBtn}>
-      <Link href={"/products"}>  <button>Continue Shopping</button></Link>
-        <button onClick={()=>cleanAllCartItem()}>Clean cart</button>
-      </div>
-
-      {/* bottom section */}
-
-      <div className={style.bottomSection}>
-        <div className={style.left}>
-          <h4>Special instructions for seller</h4>
-          <textarea></textarea>
-        </div>
-        {/* right */}
-        <div className={style.right}>
-          <h4>Cart Totals</h4>
-          <div className={style.tableSection}>
-            <li className={style.heading}>Subtotal</li>
-            <li className={style.data}>Rs. {payableAmount}</li>
-
-            <li className={style.bottomHeading}>Total</li>
-            <li className={style.bottomData}>Rs. {payableAmount}</li>
-          </div>
-
-          <Link href="/checkout"><button>Proceed to checkout</button></Link>
-        </div>
-      </div>
-      </div>
-
-
     </div>
   );
 }
--- app/checkout/page.js
+++ app/checkout/page.js
@@ -1,6 +1,65 @@
+"use client";
+import { useEffect, useState } from "react";
 import style from "./style.module.css";
-
+import { useCart } from "react-use-cart";
+import {  toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export default function page() {
+const[coupon,setCoupon]=useState('')
+
+const [onlinePaymentState,setOnlinePaymentState]=useState(true);
+  const { cartTotal, items } = useCart();
+  const [payableAmount, setPayableAmount] = useState(0);
+  useEffect(() => {
+    setPayableAmount(0);
+    let sum = 0;
+    for (let i = 0; i < items.length; i++) {
+      let number = parseInt(items[i].qtyBook * items[i].price);
+      sum += number;
+    }
+    setPayableAmount(sum);
+  }, []);
+
+
+
+
+  const checkCoupon=()=>{
+    if(coupon==""){
+      toast.error("Please Enter Coupon Code", {
+        position: "bottom-right",
+        autoClose: 1200,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+      return;
+    }
+    else{
+      toast.error("Invalid Coupon Code", {
+        position: "bottom-right",
+        autoClose: 1200,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+      });
+    }
+  }
+
+
+  const changePaymentMode=(active)=>{
+if(active=="cod"){
+  setOnlinePaymentState(false)
+  document.getElementById('cod').checked = true;
+}else{
+  setOnlinePaymentState(true)
+  document.getElementById('card').checked = true;
+
+}
+  }
   return (
     <div className={style.checkoutContainer}>
       <div className={style.formContainer}>
@@ -62,83 +121,95 @@ export default function page() {
           <p>All transactions are secure and encrypted.</p>
 
           <div className={style.payment}>
-            <li>
-              <input type="radio" name="paymentmode" />
+
+     
+            <li onClick={()=>changePaymentMode('card')} style={{cursor:'pointer'}}>
+              <input type="radio" name="paymentmode"  id="card" defaultChecked/>
               <span>Credit card</span>
             </li>
-            <li className={style.cardDetails}>
+            {onlinePaymentState&&<>
+              <li className={style.cardDetails}>
               <div className={style.formInput}>
                 <input type="text" placeholder="Card number" id="cardNumber" />
-                <input type="text" placeholder="Expiration date (MM/YY)" className={style.half}/>
-                <input type="text" placeholder="Security Code" className={style.half}/>
+                <input
+                  type="text"
+                  placeholder="Expiration date (MM/YY)"
+                  className={style.half}
+                />
+                <input
+                  type="text"
+                  placeholder="Security Code"
+                  className={style.half}
+                />
                 <input type="text" placeholder="Name on Card" />
               </div>
             </li>
+            </>}
+           
 
 
 
-            <li>
-              <input type="radio" name="paymentmode" id="" />
-              <span>Cash on Delivery (COD)</span>
+
+            <li onClick={()=>changePaymentMode('cod')} style={{cursor:'pointer'}}>
+              <input type="radio" name="paymentmode" id="cod" />
+              <span >Cash on Delivery (COD)</span>
             </li>
           </div>
 
-
           <div className={style.payNow}>
-<button type="submit">Pay Now</button>
-
+            <button type="submit">Pay Now</button>
           </div>
         </form>
       </div>
 
       {/* item container */}
       <div className={style.itemContainer}>
-      <div className={style.itemMainContainer}>
-
-
-        <div className={style.items}>
-        <div className={style.imageContainer}>
-<div className={style.qty}>1</div>
-          <img src="cart1.webp" alt="cart dummy image" />
-        </div>
-        <div className={style.titleOfItem}>
-        Aa. Men Coat
-        </div>
-        <div className={style.rs}>
-        $117.00
-        </div>
-        </div>
-
-        <div className={style.items}>
-        <div className={style.imageContainer}>
-<div className={style.qty}>1</div>
-          <img src="cart1.webp" alt="cart dummy image" />
-        </div>
-        <div className={style.titleOfItem}>
-        Aa. Men Coat
-        </div>
-        <div className={style.rs}>
-        $117.00
-        </div>
-        </div>
-
+        <div className={style.itemMainContainer}>
+          {items.length != 0 && (
+            <>
+              {items.map((item) => {
+                return (
+                  <div className={style.items} key={item.id}>
+                    <div className={style.imageContainer}>
+                      <div className={style.qty}>{item.qtyBook}</div>
+                      <img src={item.productImage} alt={item.productImage} />
+                    </div>
+                    <div className={style.titleOfItem}>{item.productName}</div>
+                    <div className={style.rs}>{item.price * item.qtyBook}</div>
+                  </div>
+                );
+              })}
+            </>
+          )}
         </div>
 
         {/* bottom total */}
         <div className={style.bottomTotal}>
-<div className={style.discount}>
-<input type="text" name="" placeholder="Discount code or gift card" />
-<button>Apply</button>
-</div>
-<div className={style.price}>
-<li><span >Subtotal</span> <span >$627.00</span></li>
+          <div className={style.discount}>
+            <input
+              type="text"
+              name="Coupon"
+              value={coupon}
+              onChange={(e)=>setCoupon(e.target.value)}
+              placeholder="Discount code or gift card"
+            />
+            <button onClick={()=>checkCoupon()}>Apply</button>
+          </div>
+          <div className={style.price}>
+            <li>
+              <span>Subtotal</span> <span>Rs {cartTotal}</span>
+            </li>
 
-<li><span >Shipping</span> <span >$0.15</span></li>
+            <li>
+              <span>Shipping</span> <span>Rs 0.0</span>
+            </li>
 
-<li><span className={style.heading}>Total</span> <span className={style.priceTotal}>
-$627.15</span></li>
-</div>
-
+            <li>
+              <span className={style.heading}>Total</span>{" "}
+              <span className={style.priceTotal}>Rs {payableAmount}</span>
+            </li>
+          </div>
+          
         </div>
       </div>
     </div>
